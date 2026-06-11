ea2129c50260a0ddd127aef5adccae72732df054
farhan <mofarhannn@gmail.com>
2026-06-11 08:39:04 +0700

    feat(about & projects): created about page and projects page

---
+++ app/pages/about.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="flex justify-center py-40 px-4 bg-cream">
+        <div class="w-full max-w-100">
+            <div class="flex flex-col gap-8">
+                <div class="flex flex-col justify-center gap-4">
+                    <h1>ABOUT ME</h1>
+                    <BaseButton to="/" variant="primary">
+                        BACK
+                    </BaseButton>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
